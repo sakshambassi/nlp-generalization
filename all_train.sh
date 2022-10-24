95261dcff1b5589ed867f4e7d2a898054799e932
Saksham Bassi <sakshambassi@gmail.com>
2022-10-24 16:30:40 -0400

    Add fix for bash errors

--- all_train.sh
+++ all_train.sh
@@ -1,2 +1,3 @@
-for lang in 'fr', 'es', 'de', 'tr', 'el', 'bg', 'ru', 'ar', 'vi', 'th', 'zh', 'hi', 'sw', 'ur'; do:
+for lang in 'fr' 'es' 'de' 'tr' 'el' 'bg' 'ru' 'ar' 'vi' 'th' 'zh' 'hi' 'sw' 'ur'; do
     sbatch ./slurms/greene_run.slurm $lang
+done
